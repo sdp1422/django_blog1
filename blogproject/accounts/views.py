f25e4167e79ca3ce35a94f548630fb576b991f3d
sdpark1422 <sdp1422@naver.com>
2019-02-14 17:48:22 +0900

    블로그 과제1
    멋쟁이사자처럼 운영진 블로그 과제1

---
+++ blogproject/accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect # redirect() import 해주어야함
+from django.contrib.auth.models import User # 유저 추가 기능
+from django.contrib import auth # 계정 추가 기능
+# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('home')
+    return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    else:
+        return render(request, 'login.html')
